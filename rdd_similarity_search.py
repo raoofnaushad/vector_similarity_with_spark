6a471504f75989739c65315333eb5a071536ec4c
accubits <raoof@accubits.com>
2020-10-23 11:42:21 +0530

    Checkign RDD

---
+++ rdd_similarity_search.py
@@ -0,0 +1,93 @@
+
+from pyspark.sql import SparkSession
+import pyspark.sql.types as T
+import pyspark.sql.functions as F
+from pyspark.context import SparkContext
+from pyspark.sql import SQLContext
+
+
+import time
+import numpy as np
+import json
+
+import config
+
+
+
+appName = "Vector Search Pyspark"
+master = 'local'
+
+
+
+def parse_embedding_from_string(x):
+    res = json.loads(x)
+    return res
+
+sc = SparkContext.getOrCreate()
+spark = SparkSession(sc)
+sqlContext = SQLContext(sc)
+
+# Create Spark session
+spark = SparkSession.builder \
+    .master(master) \
+    .appName(appName) \
+    .getOrCreate()
+    
+retrieve_embedding = F.udf(parse_embedding_from_string, T.ArrayType(T.DoubleType()))
+
+# Feature vector to data frame
+feature_df = spark.read.format('csv') \
+                .option('header',True) \
+                .option('multiLine', True) \
+                .load(config.HDFS_DATA_PATH)
+
+
+# Query vector
+query_df = spark.read.format('csv') \
+                .option('header',True) \
+                .option('multiLine', True) \
+                .load(config.HDFS_QUERY_PATH)
+               
+                             
+feature_df.show()
+feature_df.printSchema()
+query_df.show()
+query_df.printSchema()
+
+
+feature_df = feature_df.withColumn("features_new", retrieve_embedding(F.col("features")))
+feature_df.printSchema()
+query_df = query_df.withColumn("features_new", retrieve_embedding(F.col("features")))
+query_df.printSchema()
+
+value = query_df.select('features_new').collect()[0][0]
+print("Query vector is {}".format(value))
+print("--------------------*****----------------------------------")
+
+def cos_sim(vec):
+    if (np.linalg.norm(value) * np.linalg.norm(vec)) != 0:
+        dot_value = np.dot(value, vec) / (np.linalg.norm(value)*np.linalg.norm(vec))
+        return dot_value.tolist()
+
+
+start = time.time()
+
+feature_df_rdd = feature_df.rdd
+
+cos_sim_udf = F.udf(cos_sim, T.FloatType())
+
+feature_df_cos_rdd = feature_df_rdd.withColumn('cos_dis', cos_sim_udf('features_new')).dropna(subset='cos_dis')
+feature_df_cos = sqlContext.createDataFrame(feature_df_cos_rdd)
+feature_df_cos.show()
+
+max_values = feature_df_cos.select('image_paths','cos_dis').orderBy('cos_dis', ascending=False).limit(5).collect()
+top_matches = []
+for x in max_values:
+    top_matches.append(x[0])
+
+end = time.time()
+
+print("Top matches are {}".format(top_matches))
+print("Total time is {}".format(end-start))
+print("---------------------------------------------------------")
+
